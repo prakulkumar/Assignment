a536c679d5c53af1cc8bb50b733ed5e72ce44854
prakulkumar <dhiman404prakul@gmail.com>
2019-01-14 08:18:39 +0530

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'assignment';
+  block = [
+    {
+      heading: 'Thermostat',
+      subheading: 'In Bedroom',
+      imageUrl: 'assets/thermostat.png'
+    },
+    {
+      heading: 'Bed Lamp',
+      subheading: 'In Bedroom',
+      imageUrl: 'assets/lamp.png'
+    },
+    {
+      heading: 'Noria AC',
+      subheading: 'In Bedroom',
+      imageUrl: 'assets/ac.png'
+    },
+    {
+      heading: 'Door Lock',
+      subheading: 'In Home office',
+      imageUrl: 'assets/lock.png'
+    },
+    {
+      heading: 'LG TV',
+      subheading: 'In Living room',
+      imageUrl: 'assets/tv.png'
+    },
+    {
+      heading: 'Thermostat',
+      subheading: 'In Bedroom',
+      imageUrl: 'assets/thermostat.png'
+    }
+  ]
 }
